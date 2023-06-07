ce212c5aeb2c8a1fc94e898ef65fb94458b84939
Conner Van Loan <cdvanloan@gmail.com>
2023-06-07 12:30:45 -0600

    adds validations to subscription model

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,2 +1,7 @@
 class Subscription < ApplicationRecord
+  validates_presence_of :title, :price, :status, :frequency, :customer_id
+
+  belongs_to :customer
+  has_many :subscription_teas, dependent: :destroy
+  has_many :teas, through: :subscription_teas
 end
--- app/models/subscription_tea.rb
+++ app/models/subscription_tea.rb
@@ -1,2 +1,3 @@
 class SubscriptionTea < ApplicationRecord
+  
 end
