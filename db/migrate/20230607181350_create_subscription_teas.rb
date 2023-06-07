981b50a883133e8a6c93dc4cc962dd9602c6c5e4
Conner Van Loan <cdvanloan@gmail.com>
2023-06-07 13:35:48 -0600

    removes subscription teas table, edits relationships, updates testing

--- db/migrate/20230607181350_create_subscription_teas.rb
+++
@@ -1,9 +0,0 @@
-class CreateSubscriptionTeas < ActiveRecord::Migration[7.0]
-  def change
-    create_table :subscription_teas do |t|
-      t.references :subscription, foreign_key: true
-      t.references :tea, foreign_key: true
-      t.timestamps
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_07_181350) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_07_170324) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,23 +23,16 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_07_181350) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "subscription_teas", force: :cascade do |t|
-    t.bigint "subscription_id"
-    t.bigint "tea_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["subscription_id"], name: "index_subscription_teas_on_subscription_id"
-    t.index ["tea_id"], name: "index_subscription_teas_on_tea_id"
-  end
-
   create_table "subscriptions", force: :cascade do |t|
     t.bigint "customer_id"
+    t.bigint "tea_id"
     t.float "price"
     t.integer "status"
     t.integer "frequency"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["customer_id"], name: "index_subscriptions_on_customer_id"
+    t.index ["tea_id"], name: "index_subscriptions_on_tea_id"
   end
 
   create_table "teas", force: :cascade do |t|
@@ -51,7 +44,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_07_181350) do
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "subscription_teas", "subscriptions"
-  add_foreign_key "subscription_teas", "teas"
   add_foreign_key "subscriptions", "customers"
+  add_foreign_key "subscriptions", "teas"
 end
